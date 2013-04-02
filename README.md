c44e8a8290d0d47b9bc5c4746633a5b2a596b351
Simon Kågedal Reimer <skagedal@gmail.com>
2013-04-03 00:18:11 +0200

    Initial commit of svgclip tool.

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 svgclip
 =======
 
-Clip SVG files to the size of actual drawing. Uses Inkscape to get bounding box.
+Sometimes you have an SVG file and you want to reduce its size to the
+actual drawing.  I have had a hard time finding a good way to do this
+programmatically, for batch processing or other.
+
+Googling around, one method I've found is to use Inkscape. It can be
+scripted like this:
+
+    inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose *.svg
+
+That takes a long time, opens up a GUI and isn't very flexible.
+
+However, you can also get information about the bounding box with the 
+`--query-all` option to inkscape. Using this information, I wrote a python
+script that uses rsvg and cairo to write the drawing to a file with the 
+right dimensions.
+
+Since it was easy, I also added an option to add a margin to the drawing.
+
+Make sure you have Inkscape, python-rsvg and python-cairo installed
+(on Ubuntu, try `sudo apt-get install inkscape python-rsvg python-cairo`)
+and then run `svgclip.py inputfile -o outputfile [-m marginpixels]`.
+
+Regards,
+Simon Kågedal Reimer
---
+++ svgclip.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+svgclip.py
+
+Copyright (c) 2013 Simon Kågedal Reimer
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import argparse
+import subprocess
+import cairo
+import rsvg
+
+def query_svg(svgfile):
+    """Parses the output from inkscape --query-all"""
+    def parse_line(line):
+        split = line.split(',')
+        return [split[0]] + [float(x) for x in split[1:]]
+    output = subprocess.check_output(["inkscape", "--query-all", svgfile])
+    lines = output.split('\n')
+    return [parse_line(line) for line in lines]
+
+def get_bounding_box(svgfile):
+    all = query_svg(svgfile)
+    # The first line seems to always be the full drawing
+    return all[0]
+
+def print_info(svgfile):
+    bbox = get_bounding_box(svgfile)
+    print("""
+X:      %f
+Y:      %f
+Width:  %f
+Height: %f""".strip() % tuple(bbox[1:]))
+
+def clip(inputfile, outputfile, margin):
+    name, x, y, width, height = get_bounding_box(inputfile)
+    
+    svg = rsvg.Handle(file=inputfile)
+    surface = cairo.SVGSurface(outputfile, 
+                               width + margin * 2, 
+                               height + margin * 2)
+    ctx = cairo.Context(surface)
+    ctx.translate(-x + margin, -y + margin)
+    svg.render_cairo(ctx)
+    surface.finish()
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description=
+                                     'Clip SVG file to bounding box.')
+    parser.add_argument('input', 
+                        help="SVG file to read")
+    parser.add_argument('-o', '--output',
+                        help="SVG file to write",
+                        metavar="SVGFILE")
+    parser.add_argument('-m', '--margin',
+                        help="Margin to add",
+                        metavar="PIXELS",
+                        default=0,
+                        type=float)
+    return parser
+
+if __name__ == "__main__":
+    args = arg_parser().parse_args()
+    if args.output is None:
+        print_info(args.input)
+    else:
+        clip(args.input, args.output, args.margin)
+
+
+
