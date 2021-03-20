e3251eac8f13807229b197d1a0c4ab8d7c83d2a2
Fredrick Brennan <copypaste@kittens.ph>
2021-03-20 18:26:09 -0400

    import gi
    Not sure how this ever worked without this?
    
    Closes skagedal/svgclip#6.

--- svgclip.py
+++ svgclip.py
@@ -31,6 +31,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 import argparse
 import subprocess
 import cairo
+import gi
 gi.require_version('Rsvg', '2.0')
 gi.require_foreign("cairo")
 from gi.repository import Rsvg
